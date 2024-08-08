7f23068cd5028def5555b83356bfcd141a0aa223
YurijM <y-myagkov@mail.ru>
2024-08-08 22:04:23 +0300

    Проверка работы Github: "Desktop to Notebook"

--- app/src/main/java/com/mu/tote2026/MainActivity.kt
+++ app/src/main/java/com/mu/tote2026/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,7 +21,11 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             Tote2026Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
