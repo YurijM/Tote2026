e26c7d0b3b98b4fb3ea7343167e7131c16471d77
YurijM <y-myagkov@mail.ru>
2024-08-09 15:44:50 +0300

    Пробная авторизация

--- app/src/main/java/com/mu/tote2026/MainActivity.kt
+++ app/src/main/java/com/mu/tote2026/MainActivity.kt
@@ -3,13 +3,24 @@ package com.mu.tote2026
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import com.mu.tote2026.ui.theme.Tote2026Theme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,14 +33,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tote2026Theme {
                 Scaffold(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                    /*containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,*/
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen()
                 }
             }
         }
@@ -37,9 +45,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = { newValue ->
+                email = newValue
+            },
+            isError = true,
+            singleLine = true,
+            label = { Text(text = "email") },
+            placeholder = { Text(text = "Введите адрес email") },
+            leadingIcon = {
+                Icon(
+                    painterResource(id = R.drawable.ic_email),
+                    contentDescription = null
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { newValue ->
+                password = newValue
+            },
+            singleLine = true,
+            label = { Text(text = "Пароль") },
+            placeholder = { Text(text = "Введите пароль") },
+            leadingIcon = {
+                Icon(
+                    painterResource(id = R.drawable.ic_password),
+                    contentDescription = null
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    painterResource(id = R.drawable.ic_eye),
+                    contentDescription = null
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation()
+        )
+    }
 }
--- app/src/main/java/com/mu/tote2026/di/AppModule.kt
+++ app/src/main/java/com/mu/tote2026/di/AppModule.kt
@@ -1,10 +1,26 @@
 package com.mu.tote2026.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore() = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideFirebaseStorage() = FirebaseStorage.getInstance()
 }
