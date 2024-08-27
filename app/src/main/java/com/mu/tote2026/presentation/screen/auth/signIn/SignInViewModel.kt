24d240d4579e3850092f864d4e7eb7f17bf0691a
YurijM <y-myagkov@mail.ru>
2024-08-27 21:09:58 +0300

    Авторизация -> OnSignIn: collect заменён на onEach

--- app/src/main/java/com/mu/tote2026/presentation/screen/auth/signIn/SignInViewModel.kt
+++ app/src/main/java/com/mu/tote2026/presentation/screen/auth/signIn/SignInViewModel.kt
@@ -13,7 +13,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
@@ -49,14 +50,14 @@ class SignInViewModel @Inject constructor(
                 enabledButton = checkValues()
             }
             is SignInEvent.OnSignIn -> {
-                viewModelScope.launch {
+                /*viewModelScope.launch {
                     authUseCase.signIn(email, password).collect { signState ->
                         _state.value = SignInState(signState)
                     }
-                }
-                /*authUseCase.signIn(email, password).onEach {
-                    _state.value = SignInState(it)
-                }.launchIn(viewModelScope)*/
+                }*/
+                authUseCase.signIn(email, password).onEach { signState ->
+                    _state.value = SignInState(signState)
+                }.launchIn(viewModelScope)
             }
         }
     }
