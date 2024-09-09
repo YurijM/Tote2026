9aaf06d8a1bc3ffe152eafcb62ef8a3cf585d3a2
YurijM <y-myagkov@mail.ru>
2024-09-09 11:57:48 +0300

    navigationToMain: AUTH_SCREEN заменён на SPLASH_SCREEN

--- app/src/main/java/com/mu/tote2026/presentation/navigation/destination/main/ToMainDestination.kt
+++ app/src/main/java/com/mu/tote2026/presentation/navigation/destination/main/ToMainDestination.kt
@@ -1,12 +1,12 @@
 package com.mu.tote2026.presentation.navigation.destination.main
 
 import androidx.navigation.NavController
-import com.mu.tote2026.presentation.utils.Route.AUTH_SCREEN
 import com.mu.tote2026.presentation.utils.Route.MAIN_SCREEN
+import com.mu.tote2026.presentation.utils.Route.SPLASH_SCREEN
 
 fun NavController.navigateToMain() {
     navigate(MAIN_SCREEN) {
-        popUpTo(AUTH_SCREEN) {
+        popUpTo(SPLASH_SCREEN) {
             inclusive = true
         }
     }
