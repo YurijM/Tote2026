64e89d9ae2419ed401a8c08497ae63bdfd6330da
YurijM <y-myagkov@mail.ru>
2025-02-11 16:22:44 +0300

    На экран списка участников в админке добавлена информация о разыгранной и оставшейся суммах.

--- app/src/main/java/com/mu/tote2026/presentation/screen/admin/gambler/list/AdminGamblerListScreen.kt
+++ app/src/main/java/com/mu/tote2026/presentation/screen/admin/gambler/list/AdminGamblerListScreen.kt
@@ -1,6 +1,8 @@
 package com.mu.tote2026.presentation.screen.admin.gambler.list
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,7 +21,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mu.tote2026.R
 import com.mu.tote2026.domain.model.GamblerModel
@@ -28,6 +36,7 @@ import com.mu.tote2026.presentation.components.Title
 import com.mu.tote2026.presentation.utils.toLog
 import com.mu.tote2026.ui.common.UiState
 
+@SuppressLint("DefaultLocale")
 @Composable
 fun AdminGamblerListScreen(
     toGamblerEdit: (String) -> Unit
@@ -66,9 +75,80 @@ fun AdminGamblerListScreen(
         modifier = Modifier.fillMaxSize()
     ) {
         Title(stringResource(R.string.admin_gambler_list))
-        Text(
-            text = stringResource(R.string.prize_fund, viewModel.prizeFund)
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.prize_fund) + ":",
+                textAlign = TextAlign.End,
+                lineHeight = 0.85.em,
+                modifier = Modifier
+                    .padding(end = 4.dp)
+                    .weight(1.35f)
+            )
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(fontWeight = FontWeight.Bold)
+                    ) {
+                        append(viewModel.prizeFund.toString())
+                    }
+                    append(" руб.")
+                },
+                lineHeight = 0.85.em,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.winner_played) + ":",
+                textAlign = TextAlign.End,
+                lineHeight = 0.85.em,
+                modifier = Modifier
+                    .padding(end = 4.dp)
+                    .weight(1.35f)
+            )
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(fontWeight = FontWeight.Bold)
+                    ) {
+                        append(String.format("%.2f", viewModel.winnerPlayed))
+                    }
+                    append(" руб.")
+                },
+                lineHeight = 0.85.em,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 4.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.rested_prize_fund) + ":",
+                textAlign = TextAlign.End,
+                lineHeight = 0.85.em,
+                modifier = Modifier
+                    .padding(end = 4.dp)
+                    .weight(1.35f)
+            )
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(fontWeight = FontWeight.Bold)
+                    ) {
+                        append(String.format("%.2f", viewModel.restedPrizeFund))
+                    }
+                    append(" руб.")
+                },
+                lineHeight = 0.85.em,
+                modifier = Modifier.weight(1f)
+            )
+        }
         HorizontalDivider(
             thickness = 1.dp,
             color = MaterialTheme.colorScheme.primary,
--- app/src/main/java/com/mu/tote2026/presentation/screen/admin/gambler/list/AdminGamblerListViewModel.kt
+++ app/src/main/java/com/mu/tote2026/presentation/screen/admin/gambler/list/AdminGamblerListViewModel.kt
@@ -1,25 +1,42 @@
 package com.mu.tote2026.presentation.screen.admin.gambler.list
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mu.tote2026.data.repository.GROUP_GAMES_COUNT
+import com.mu.tote2026.data.repository.PLAYOFF_GAMES_COUNT
+import com.mu.tote2026.domain.model.CommonParamsModel
 import com.mu.tote2026.domain.model.GamblerModel
 import com.mu.tote2026.domain.usecase.gambler_usecase.GamblerUseCase
+import com.mu.tote2026.domain.usecase.game_usecase.GameUseCase
 import com.mu.tote2026.ui.common.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import java.lang.System.currentTimeMillis
 import javax.inject.Inject
 
 @HiltViewModel
 class AdminGamblerListViewModel @Inject constructor(
-    gamblerUseCase: GamblerUseCase
+    private val gamblerUseCase: GamblerUseCase,
+    private val gameUseCase: GameUseCase,
 ) : ViewModel() {
     private val _state: MutableStateFlow<AdminGamblerListState> = MutableStateFlow(AdminGamblerListState())
     val state = _state.asStateFlow()
 
     var prizeFund = 0
+    var winnerPlayed by mutableDoubleStateOf(0.0)
+        private set
+    var restedPrizeFund by mutableDoubleStateOf(0.0)
+        private set
+    private var matchesPlayed = 0
+    private var matchesPlayedbyGroup = 0
+    private var matchesPlayedbyPlayoff = 0
+    private var commonParams = CommonParamsModel()
 
     init {
         gamblerUseCase.getGamblerList().onEach { gamblerListState ->
@@ -29,8 +46,39 @@ class AdminGamblerListViewModel @Inject constructor(
                 )
                 _state.value = gamblers
                 prizeFund = gamblerListState.data.sumOf { it.rate }
+                calc()
             } else {
                 _state.value = AdminGamblerListState(gamblerListState)
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun calc() {
+        gameUseCase.getGameList().onEach { gameListState ->
+            if (gameListState is UiState.Success) {
+                matchesPlayed = gameListState.data.filter { it.start.toLong() < currentTimeMillis() }.size
+                if (matchesPlayed > GROUP_GAMES_COUNT) {
+                    matchesPlayedbyGroup = GROUP_GAMES_COUNT
+                    matchesPlayedbyPlayoff = matchesPlayed - matchesPlayedbyGroup
+                } else {
+                    matchesPlayedbyGroup = matchesPlayed
+                    matchesPlayedbyPlayoff = 0
+                }
+
+                gamblerUseCase.getCommonParams().onEach { commonParamsState ->
+                    if (commonParamsState is UiState.Success) {
+                        commonParams = commonParamsState.data
+                        winnerPlayed = matchesPlayedbyGroup * (commonParams.groupPrizeFund / GROUP_GAMES_COUNT) +
+                                matchesPlayedbyPlayoff * (commonParams.playoffPrizeFund / PLAYOFF_GAMES_COUNT) +
+                                commonParams.winnersPrizeFundByStake +
+                                commonParams.winnersPrizeFund
+                        restedPrizeFund = commonParams.prizeFund -
+                                matchesPlayedbyGroup * (commonParams.groupPrizeFund / GROUP_GAMES_COUNT) -
+                                matchesPlayedbyPlayoff * (commonParams.playoffPrizeFund / PLAYOFF_GAMES_COUNT) -
+                                commonParams.winnersPrizeFundByStake -
+                                commonParams.winnersPrizeFund
+                    }
+                }.launchIn(viewModelScope)
             }
         }.launchIn(viewModelScope)
     }
